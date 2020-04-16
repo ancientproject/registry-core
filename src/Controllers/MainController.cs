b6f6c0f840e763a5eb6541ecd37be24f17ac58c2
Yuuki Wesp <ls-micro@ya.ru>
2020-04-16 23:15:17 +0300

    fix

--- src/Controllers/MainController.cs
+++ src/Controllers/MainController.cs
@@ -1,9 +1,10 @@
 ﻿namespace registry.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
-
+    [ApiController]
     public class MainController : ControllerBase
     {
+        [HttpGet("/")]
         public IActionResult Index() => Ok("kukareku");
     }
 }
